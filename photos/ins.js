8f0e47180d859f1ceba41f21f94a9f810408ff13
骑着猪上高速 <73281353+chenchen1314@users.noreply.github.com>
2020-12-29 12:43:21 +0800

    Site updated: 2020-12-29 12:43:19

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/chenchen1314/photos/main/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/chenchen1314/photos/main/photos/' + data.link[i];
+          var minSrc = 'https://gitee.com/xiaochen1314/photos/blob/main/min_photos/' + data.link[i];
+          var src = 'https://gitee.com/xiaochen1314/photos/blob/main/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
